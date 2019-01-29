44ef1449963fb4577e783613ee2c18a560d7f3c7
Amol Grover <29180487+frextrite@users.noreply.github.com>
2019-01-29 12:38:37 +0530

    Add function to convert base64 string to an image

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 const express = require('express');
+const fs = require('fs');
 const app = express()
 const port = 3000;
 
 app.get('/', (req, res) => res.send('Hello, World!'))
+
+function convertToImage(base64String) {
+  fs.writeFile('image.jpg', base64String, {encoding: 'base64'}, (error) => {
+    console.log(error);
+  })
+}
 
 app.listen(port, () => console.log(`Listening on port ${port}\n`))
