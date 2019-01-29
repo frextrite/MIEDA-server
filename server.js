a757144ddff047d07df1e6d7acc43f4bf27779fb
Amol Grover <29180487+frextrite@users.noreply.github.com>
2019-01-29 21:48:46 +0530

    Add function which calls face API and returns the obtained result

--- server.js
+++ server.js
@@ -1,23 +1,24 @@
 const express = require('express');
 const moment = require('moment');
+const axios = require('axios');
 const fs = require('fs');
 const app = express()
 const port = 3000;
 
 // Face API Configuration
-const subscriptionKey = process.env.faceAPISubscriptionKey;
+const subscriptionKey = process.env.FACEAPISUBSCRIPTIONKEY;
 
-const uriBase = 'https://westcentralus.api.cognitive.microsoft.com/face/v1.0/detect';
+const apiURL = 'https://westeurope.api.cognitive.microsoft.com/face/v1.0/detect';
 
 const params = {
   returnFaceId: true,
   returnFaceLandmarks: false,
-  returnFaceAttributes: 'age, gender, smile, emotion'
+  returnFaceAttributes: 'age,gender,headPose,smile,facialHair,glasses,' +
+                        'emotion,hair,makeup,occlusion,accessories,blur,exposure,noise'
 };
 
 const options = {
-  uri: uriBase,
-  qs: params,
+  params: params,
   headers: {
     'Content-Type': 'application/octet-stream',
     'Ocp-Apim-Subscription-Key' : subscriptionKey
@@ -39,6 +40,23 @@ function getFileName() {
   let filename = `IMG_${datetime}.jpg`;
   console.log(filename);
   return filename;
+}
+
+function getEmotionData(filename) {
+  // create image stream
+  let image = fs.createReadStream(filename);
+
+  // send POST request to face API
+  axios.post(apiURL, image, options)
+    .then((response) => {
+      // response.data is an array of detected faces
+      console.log(response.data[0]);
+      return response;
+    })
+    .catch((error) => {
+      console.log('Caught error', error);
+    }
+  );
 }
 
 app.listen(port, () => console.log(`Listening on port ${port}\n`))
